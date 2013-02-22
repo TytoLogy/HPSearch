2184cb37b160659ef6a6c804ca5658bed968c1bb
Sharad Shanbhag <sshanbhag@neomed.edu>
2013-02-22 00:43:16 -0500

    scaling work continues
    need to figure out scaling!!!!!

--- caltest/caltest.m
+++ caltest/caltest.m
@@ -1,27 +1,42 @@
-calfile = 'C:\TytoLogy\Calibration\CalibrationData\LabUser\test_1-10kHz.cal';
+calfile = ['C:\TytoLogy\Calibration\CalibrationData\' username ...
+						'\test.cal'];
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% load settings
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%----------------------------------------------------------------
+%% load settings
+%----------------------------------------------------------------
 caltest_settings
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% load calibration data
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%----------------------------------------------------------------
+%% load calibration data
+%----------------------------------------------------------------
 caldata = load_headphone_cal(calfile);
 caldata = calchancopy(caldata, 2, 1);
+iodev.Fs = 100000;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% TDT init
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%----------------------------------------------------------------
+%% TDT init
+%----------------------------------------------------------------
 tdtinit;
 
-% Synthesize noise and get rms values for attenuator
-[Sntest, rmsvaltest] = syn_headphone_noise(StimDuration, iodev.Fs, Fmin, Fmax, 0, 100, caldata);
+%----------------------------------------------------------------
+%% Synthesize noise and get rms values for attenuator
+%----------------------------------------------------------------
+[Sn, rmsval] = syn_headphone_noise(StimDuration, iodev.Fs, Fmin, Fmax, 0, 100, caldata);
+spl_val = 20*[1 1];
+figure(100);
+plot(Sn(R, :), 'r')
+fprintf('\n\nSntest RMS: %f\n', rmsval(R));
+fprintf('spl desired: %d\n', spl_val(R));
+fprintf('Predicted, zeroatten db SPL: %f\n', dbspl(VtoPa * rmsval(R)));
+fprintf('atten (db): %f\n', caldata.mindbspl(R) + db(rmsval(R)) - spl_val(R));
+fprintf('atten (db SPL): %f\n', caldata.mindbspl(R) + dbspl(VtoPa*rmsval(R)) - spl_val(R));
+fprintf('atten2 (db SPL): %f\n', dbspl(VtoPa*rmsval(R)) - spl_val(R));
+atten = figure_headphone_atten(spl_val, rmsval, caldata);
+fprintf('figure atten (db): %f\n', atten(R));
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% set the start and end bins for the data acquisition
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%----------------------------------------------------------------
+%% set the start and end bins for the data acquisition
+%---------------------------------------------------------------- 
 zerostim = syn_null(StimDuration, iodev.Fs, MONO);
 % get the # of points to send out and to collect
 acqpts = ms2samples(AcqDuration, iodev.Fs);
@@ -34,9 +49,9 @@ stim_end = stim_start + ms2samples(StimDuration, iodev.Fs);
 start_bin = stim_start + ms2samples(StimRamp, iodev.Fs);
 end_bin = stim_end - ms2samples(StimRamp, iodev.Fs);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% setup storage for data
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%---------------------------------------------------------------- 
+%% setup storage for data
+%---------------------------------------------------------------- 
 % data.mags = zeros(Nchannels, Nspeakers);
 % 	data.phis = cell(Nchannels, Nspeakers);
 % 	data.stim = cell(Nreps, 1);
@@ -51,9 +66,9 @@ end_bin = stim_end - ms2samples(StimRamp, iodev.Fs);
 % 	data.atten = zeros(Nreps, Nspeakers);
 % 	data.rmsvals = zeros(Nreps, Nspeakers);
 	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% setup plots
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%---------------------------------------------------------------- 
+%% setup plots
+%---------------------------------------------------------------- 
 figure(10)
 nplots = 2;
 h.stimplot = subplot(nplots, 1, 1);
@@ -62,12 +77,14 @@ h.micplot = subplot(nplots, 1, 2);
 % h.PSDL = subplot(211);
 % h.PSDR = subplot(212);
 	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Now initiate sweeps
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-spl_val = 50*[1 1];
+%---------------------------------------------------------------- 
+%% Now initiate sweeps
+%---------------------------------------------------------------- 
+spl_val = 45*[1 1];
 Fmin = 1000;
-Fmax = 3000;
+Fmax = 14000;
+atten = [120 120];
+
 
 % loop through reps
 for rep = 1:Nreps
@@ -76,13 +93,20 @@ for rep = 1:Nreps
 	[Sn, rmsval] = syn_headphone_noise(StimDuration, iodev.Fs, Fmin, Fmax, 0, 100, caldata);
 	% ramp the sound on and off (important!)
 	Sn = sin2array(Sn, StimRamp, iodev.Fs);
-	axes(h.stimplot),	plot(Sn(R, :), 'r');
+	plot(h.stimplot, Sn(R, :), 'r');
 
 % 	data.rmsvals(rep, :) = rmsval;
 % 	data.stim{rep} = Sn;
 
 	% get the attenuator settings for the desired SPL
-	atten = figure_headphone_atten(spl_val, rmsval, caldata);
+	% 	atten = figure_headphone_atten(spl_val, rmsval, caldata);
+
+	predicted_spl = dbspl(VtoPa*rmsval(R));
+	atten(R) = predicted_spl - spl_val(R);
+	if atten(R) <= 0
+		atten(R) = 0;
+		fprintf('requested rspl too high\n')
+	end
 	atten(L) = MAX_ATTEN;
 	% set the attenuators
 	PA5setatten(PA5L, atten(L));
@@ -104,9 +128,17 @@ for rep = 1:Nreps
 % 	data.resp{rep, L} = cell2mat(resp');
 
 	% display some things
-	fprintf('%d dB  rep:%d   Ratten: %.2f   Rrms: %.4f   ref mic dB: %.2f\n', ...
-							spl_val(R), rep, atten(R), rmsval(R), dbspl(VtoPa*rmag));
-
+	fprintf('-------------------------------------------------------\n')
+	fprintf('-------------------------------------------------------\n')
+	fprintf('REP %d:\n\n', rep);
+	fprintf('\tDesired dB: %d \t Ratten: %.2f \t Stim rms: %.4f \n', spl_val(R), atten(R), rmsval(R));
+	fprintf('\tPredicted dB SPL: %.2f\n', dbspl(VtoPa*rmsval(R)))
+	fprintf('\tActual mic dB SPL: %.2f\n', dbspl(VtoPa*rmag))
+	fprintf('\tError from desired: %.2f\n', dbspl(VtoPa*rmag) - spl_val(R))
+	fprintf('\tPrediction Error: %.2f\n',  dbspl(VtoPa*rmsval(R)) - dbspl(VtoPa*rmag))
+	fprintf('-------------------------------------------------------\n')
+	fprintf('-------------------------------------------------------\n\n\n')
+	
 	plot(h.micplot, tvec, resp{R}, 'r');
 
 	[St, Smag, Sphi, F] = fftdbplot(resp{2}, iodev.Fs, figure(20));
@@ -120,14 +152,14 @@ end
 
 % data.f = Freqs;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Exit gracefully (close TDT objects, etc)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%---------------------------------------------------------------- 
+%% Exit gracefully (close TDT objects, etc)
+%---------------------------------------------------------------- 
 tdtexit;
 	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Process data
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%---------------------------------------------------------------- 
+%% Process data
+%---------------------------------------------------------------- 
 % 	data.Ldb_refmic = dbspl(VtoPa*mean(data.mags{REF, L}'));
 % 	data.Ldb_lmic = dbspl(VtoPa * mean(data.mags{L, L}')./frdata.lmagadjval);
 % 	data.Ldb_rmic = dbspl(VtoPa * mean(data.mags{R, L}')./frdata.rmagadjval);
@@ -148,9 +180,9 @@ tdtexit;
 % 	data.ITD_lmic = (data.Rphase_lmic - data.Lphase_lmic)./data.f;
 % 	data.ITD_rmic = (data.Rphase_rmic - data.Lphase_rmic)./data.f;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% PLOTS
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%---------------------------------------------------------------- 
+%% PLOTS
+%---------------------------------------------------------------- 
 % 	rep = 1;
 % 	axes(h.PSDL)
 % 	pwelch(data.resp{rep, L}(L, :), length(data.resp{rep, L}(L, :)), [], [], iodev.Fs)
@@ -158,9 +190,9 @@ tdtexit;
 % 	pwelch(data.resp{rep, R}(R, :), length(data.resp{rep, R}(R, :)), [], [], iodev.Fs)
 % 	data
 	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% save handles and data and temp file
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
+%---------------------------------------------------------------- 
+%% save handles and data and temp file
+%---------------------------------------------------------------- 	
 % 	if exist('calsoundfilename', 'var')
 % 		if ischar(calsoundfilename)
 % 			save(calsoundfilename, 'data', 'caldata', 'frdata', '-mat');
